b1884b8a06eb9ec183ae8adb305de7c0c7b24617
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-12-29 18:03:26 +0300

    mark as transactional

--- src/main/java/gva/repository/EventRepository.java
+++ src/main/java/gva/repository/EventRepository.java
@@ -3,11 +3,13 @@ package gva.repository;
 import gva.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByTimeStampIsAfter(LocalDateTime ts);
 
