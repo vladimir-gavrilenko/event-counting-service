f152904e9fb3fbabe04384d0453494131e087184
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-12-29 14:56:03 +0300

    spring-data find and count by timestamp

--- src/main/java/gva/repository/EventRepository.java
+++ src/main/java/gva/repository/EventRepository.java
@@ -4,6 +4,12 @@ import gva.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByTimeStampGreaterThan(LocalDateTime ts);
+
+    Long countByTimeStampGreaterThan(LocalDateTime ts);
 }
