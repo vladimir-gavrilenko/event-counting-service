ae6cbb5f093a9f86f860bb1d2a454ac6b4b963c2
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-12-29 17:40:18 +0300

    adding event in controller

--- src/main/java/gva/controler/EventController.java
+++ src/main/java/gva/controler/EventController.java
@@ -1,20 +1,36 @@
 package gva.controler;
 
 import gva.domain.Event;
+import gva.dto.EventDto;
+import gva.service.DateTimeService;
 import gva.service.EventService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
     private final EventService eventService;
+    private final DateTimeService dateTimeService;
 
     @GetMapping("/all")
     public List<Event> findAll() {
         return eventService.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Event> count(@Valid @RequestBody EventDto eventDto) {
+        Event event = new Event();
+        event.setTimeStamp(dateTimeService.now());
+        event.setDescription(eventDto.getDescription());
+        event = eventService.add(event);
+        return new ResponseEntity<Event>(event, HttpStatus.OK);
     }
 }
