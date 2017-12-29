bdd7eb133aee4ac89ef7d88655fff6b825a21930
Vladimir Gavrilenko <gavrilenko92@gmail.com>
2017-12-29 13:13:26 +0300

    initial

---
+++ src/main/java/gva/CountingApplication.java
@@ -0,0 +1,12 @@
+package gva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountingApplication.class, args);
+    }
+}
